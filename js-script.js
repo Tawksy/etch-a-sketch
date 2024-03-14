061d785038983e15d3adad78d091bcb74364a849
Tawksy <99350169+Tawksy@users.noreply.github.com>
2024-03-14 15:38:02 +0100

    Added New Grid Size Button
    - Added button to create new grid
    - Clicking and entering a number will create a new grid
    - Added safety to prohibit entering invalid numbers (<= 0)
    - Centered the content

--- js-script.js
+++ js-script.js
@@ -1,9 +1,17 @@
+// Applying flex to main body 
+const mainbody = document.getElementById('mainbody');
+mainbody.style.display = 'flex';
+mainbody.style.justifyContent = 'center';
+
+//Creating and applying Flex to main container
 const container = document.getElementById('container');
 container.style.width = '640px';
 container.style.height = '640px';
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.justifyContent = 'space-between';
+container.style.justifyContent = 'center';
+container.style.alignContent = 'center';
+mainbody.appendChild(container);
 
 // create 64 divs
 for (let i = 0; i < 256; i++){
@@ -18,6 +26,43 @@ for (let i = 0; i < 256; i++){
         this.style.backgroundColor = 'red'; 
     });
 }
+
+//creating the button to reset the grid
+const button_reset = document.createElement('button');
+button_reset.textContent = 'Create new Grid';
+button_reset.style.height = '24px';
+mainbody.appendChild(button_reset);
+
+button_reset.addEventListener("click", function() {
+    let usergrid;
+    do {
+        usergrid = prompt("Enter new grid size (per side)");
+        if (usergrid <= '0') {
+            alert("Invalid");
+        }
+    } while (usergrid <= '0');
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Creating the new Grid with userinput
+    const gridSize = parseInt(usergrid);
+    for (let i = 0; i < gridSize; i++){
+        const newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+        newDiv.style.backgroundColor = 'black';
+        newDiv.style.width = '32px';
+        newDiv.style.height = '32px';
+        newDiv.style.margin = '4px';
+
+        newDiv.addEventListener("mouseenter", function() {
+            this.style.backgroundColor = 'red'; 
+        });
+    }
+
+})
+
 
 
 
