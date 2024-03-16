91733ef2d428911b1ad26f8c3e44498d3aeb74e1
Tawksy <99350169+Tawksy@users.noreply.github.com>
2024-03-16 16:10:53 +0100

    Update js-script.js

--- js-script.js
+++ js-script.js
@@ -2,26 +2,26 @@
 const mainbody = document.getElementById('mainbody');
 mainbody.style.display = 'flex';
 mainbody.style.justifyContent = 'center';
+mainbody.style.alignContent = 'center';
 
 //Creating and applying Flex to main container
 const container = document.getElementById('container');
-container.style.width = '640px';
-container.style.height = '640px';
+container.style.width = '600px';
+container.style.height = '600px';
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.justifyContent = 'center';
-container.style.alignContent = 'center';
+container.style.justifyContent = 'space-between';
+container.style.alignContent = 'space-between';
 mainbody.appendChild(container);
 
-// create 64 divs
+// create 16*16 grid of divs
 for (let i = 0; i < 256; i++){
     const newDiv = document.createElement('div');
     container.appendChild(newDiv);
     newDiv.style.backgroundColor = 'black';
-    newDiv.style.width = '32px';
-    newDiv.style.height = '32px';
-    newDiv.style.margin = '4px';
-
+    newDiv.style.margin = '2px';
+    newDiv.style.height = '8px';
+    newDiv.style.width = '8px';
     newDiv.addEventListener("mouseenter", function() {
         this.style.backgroundColor = 'red'; 
     });
@@ -37,31 +37,31 @@ button_reset.addEventListener("click", function() {
     let usergrid;
     do {
         usergrid = prompt("Enter new grid size (per side)");
-        if (usergrid <= '0') {
-            alert("Invalid");
+        if (!usergrid || isNaN(parseInt(usergrid)) || parseInt(usergrid) <= 0) {
+            alert("Invalid input. Please enter a valid positive number.");
         }
-    } while (usergrid <= '0');
+    } while (!usergrid || isNaN(parseInt(usergrid)) || parseInt(usergrid) <= 0);
 
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
 
     // Creating the new Grid with userinput
-    const gridSize = parseInt(usergrid);
-    for (let i = 0; i < gridSize; i++){
+    const gridSize = parseInt(usergrid) * parseInt(usergrid);
+    console.log(gridSize);
+    for (let i = 0; i < gridSize; i++) {
         const newDiv = document.createElement('div');
         container.appendChild(newDiv);
         newDiv.style.backgroundColor = 'black';
-        newDiv.style.width = '32px';
-        newDiv.style.height = '32px';
-        newDiv.style.margin = '4px';
+        newDiv.style.flexGrow = '1';
+        newDiv.style.margin = '1px';
 
         newDiv.addEventListener("mouseenter", function() {
             this.style.backgroundColor = 'red'; 
         });
     }
-
 })
+
 
 
 
